7bc68a40a8cd890e1f3fda664804660ddb49e0f3
Michael V Castiglia (STUDENT) <S26269514@palmbeach.k12.fl.us>
2019-09-27 11:00:47 -0400

    Problem 1 Draft (loopingAdvancedAssignment)

--- loopingAdvancedAssignment/src/loopingAdvancedAssignment/Problem1.java
+++ loopingAdvancedAssignment/src/loopingAdvancedAssignment/Problem1.java
@@ -1,7 +1,21 @@
 package loopingAdvancedAssignment;
+import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("----- Welcome to Numbers Thing! -----\n");
+        System.out.print("Enter a Number: ");
+        int num = scan.nextInt();
+        double in;
+        double sum;
+        for(int count = 0; count < num; count++) {
+            System.out.print("Enter a Number: ");
+            in = scan.nextDouble();
+            sum += in;
+        }
+        System.out.print("Sum: " + sum);
+
+        scan.close();
     }
 }
--- loopingAdvancedAssignment/src/loopingAdvancedAssignment/Problem3.java
+++ loopingAdvancedAssignment/src/loopingAdvancedAssignment/Problem3.java
@@ -1,7 +1,8 @@
 package loopingAdvancedAssignment;
+import java.util.Scanner;
 
 public class Problem3 {
     public static void main(String[] args) {
-        
+
     }
 }
