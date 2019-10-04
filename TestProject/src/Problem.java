8f095c11c2500745c925ea8468a7124f85b03e65
Michael V Castiglia (STUDENT) <S26269514@palmbeach.k12.fl.us>
2019-10-04 10:10:27 -0400

    Testing Stuff

---
+++ TestProject/src/Problem.java
@@ -0,0 +1,8 @@
+public class Problem {
+    public static void main(String[] args) {
+        int i = Integer.MAX_VALUE;
+        System.out.println(i);
+        i = Integer.MIN_VALUE;
+        System.out.println(i);
+    }
+}
