f6a471e53cf4d9a87fa6a3b1fc7ce114bc6a2a09
Michael V Castiglia (STUDENT) <S26269514@palmbeach.k12.fl.us>
2019-10-24 10:30:01 -0400

    Starting CustomerOrder Class

---
+++ unit4Invoice/src/unit4Invoice/CustomerOrder.java
@@ -0,0 +1,14 @@
+package unit4Invoice;
+
+public class CustomerOrder extends Invoice {
+    /**
+     * @author Michael Castiglia
+     * AP Computer Science A
+     * CustomerOrder Class Assignment
+     * Optional Assignment
+     * Additional Reinforcement
+     */
+
+     private String name;
+
+}
