5c7e254e7f0dbdc27abb0f44869b686db21cb569
Michael Castiglia <22685648+MichaelRP1@users.noreply.github.com>
2020-02-04 10:47:31 -0500

    2015 Problem 1 FRAPPY

---
+++ frappys/src/frappys/AP2015Problem1.java
@@ -0,0 +1,42 @@
+package frappys;
+
+public class AP2015Problem1 {
+    private static int arraySum(int[] arr) {
+        int sum = 0;
+        for(int val: arr) {
+            sum += val;
+        }
+        return sum;
+    }
+
+    private static int[] rowSums(int[][] arr) {
+        int[] output = new int[arr.length];
+        for(int r = 0; r < arr.length; r++) {
+            int sum = 0;
+            for(int c = 0; c < arr[r].length; c++) {
+                sum += arr[r][c];
+            }
+            output[r] = sum;
+        }
+        return output;
+    }
+
+    private static boolean isDiverse(int[][] arr) {
+        int[] sums = rowSums(arr);
+        for(int i = 0; i < sums.length; i++) {
+            for(int j = 0; j < sums.length; j++) {
+                if((sums[i] == sums[j]) && (i != j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Not part of FRAPPY, just eliminating errors
+    public static void main(String[] args) {
+        int[][] mat1 = {{1, 1, 5, 3, 4}, {12, 7, 6, 1, 9}, {8, 11, 10, 2, 5}, {3, 2, 3, 0, 6}};
+        System.out.print(isDiverse(mat1));
+        System.out.print(arraySum(mat1[0]));
+    }
+}
